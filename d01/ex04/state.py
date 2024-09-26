a6b7b7d0daf921dd6c081090c18b00bb0b7ccdb6
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2024-09-26 23:55:24 +0200

    d01 | tabs > spaces

--- d01/ex04/state.py
+++ d01/ex04/state.py
@@ -2,17 +2,22 @@ import sys
 
 
 def findState(capital_city: str):
-    capital_city = capital_city.capitalize()
-    states = {"Oregon": "OR", "Alabama": "AL", "New Jersey": "NJ", "Colorado": "CO"}
-    capital_cities = {"OR": "Salem", "AL": "Montgomery", "NJ": "Trenton", "CO": "Denver"}
-    if capital_city in capital_cities.values():
-        for state, abbreviation in states.items():
-            if capital_cities[abbreviation] == capital_city:
-                return state
-    else:
-        return "Unknown capital city"
+	capital_city = capital_city.capitalize()
+	states = {"Oregon": "OR", "Alabama": "AL", "New Jersey": "NJ", "Colorado": "CO"}
+	capital_cities = {
+		"OR": "Salem",
+		"AL": "Montgomery",
+		"NJ": "Trenton",
+		"CO": "Denver",
+	}
+	if capital_city in capital_cities.values():
+		for state, abbreviation in states.items():
+			if capital_cities[abbreviation] == capital_city:
+				return state
+	else:
+		return "Unknown capital city"
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        print(findState(sys.argv[1]))
+	if len(sys.argv) == 2:
+		print(findState(sys.argv[1]))
--- d01/ex06/my_sort.py
+++ d01/ex06/my_sort.py
@@ -1,30 +1,30 @@
 def sortByYearAndName():
-    d = {
-        "Hendrix": "1942",
-        "Allman": "1946",
-        "King": "1925",
-        "Clapton": "1945",
-        "Johnson": "1911",
-        "Berry": "1926",
-        "Vaughan": "1954",
-        "Cooder": "1947",
-        "Page": "1944",
-        "Richards": "1943",
-        "Hammett": "1962",
-        "Cobain": "1967",
-        "Garcia": "1942",
-        "Beck": "1944",
-        "Santana": "1947",
-        "Ramone": "1948",
-        "White": "1975",
-        "Frusciante": "1970",
-        "Thompson": "1949",
-        "Burton": "1939",
-    }
-    return sorted(d.items(), key=lambda item: (item[1], item[0]))
+	d = {
+		"Hendrix": "1942",
+		"Allman": "1946",
+		"King": "1925",
+		"Clapton": "1945",
+		"Johnson": "1911",
+		"Berry": "1926",
+		"Vaughan": "1954",
+		"Cooder": "1947",
+		"Page": "1944",
+		"Richards": "1943",
+		"Hammett": "1962",
+		"Cobain": "1967",
+		"Garcia": "1942",
+		"Beck": "1944",
+		"Santana": "1947",
+		"Ramone": "1948",
+		"White": "1975",
+		"Frusciante": "1970",
+		"Thompson": "1949",
+		"Burton": "1939",
+	}
+	return sorted(d.items(), key=lambda item: (item[1], item[0]))
 
 
 if __name__ == "__main__":
-    dict = sortByYearAndName()
-    for key, value in dict:
-        print(f"{key}")
+	dict = sortByYearAndName()
+	for key, value in dict:
+		print(f"{key}")
