af4f0c81f1ec3ff490c8b45543f922da0c79c273
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2024-10-27 18:04:01 +0100

    d04 | ex00: old files removed

--- d04/ex00/d05/ex00/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-
-def ex00(request):
-	template = loader.get_template("index.html")
-	return HttpResponse(template.render())
